64de719b393509bf40f627ae74ca94e4bfa4e71d
fetrentin7 <fetrentin7@gmail.com>
2025-01-19 21:42:33 -0300

    hm

--- unit4/Assets/Scripts/PlayerController.cs
+++ unit4/Assets/Scripts/PlayerController.cs
@@ -3,17 +3,20 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
+    private GameObject focalPoint;
     private Rigidbody playerRb;
+
     public float speed = 5.0f;
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
     }
 
     
     void Update()
     {
         float forwardInput = Input.GetAxis("Vertical");
-        playerRb.AddForce(Vector3.forward * speed * forwardInput);
+        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
     }
 }
