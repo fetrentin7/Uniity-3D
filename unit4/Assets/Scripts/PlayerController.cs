2499a3b967f69f0f9806df1ecd1d320b968859e7
fetrentin7 <fetrentin7@gmail.com>
2025-01-21 17:37:26 -0300

    .

--- unit4/Assets/Scripts/PlayerController.cs
+++ unit4/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
     private GameObject focalPoint;
     private Rigidbody playerRb;
 
+    public bool hasPowerUp; //know when the powerup is picked
     public float speed = 5.0f;
     void Start()
     {
@@ -18,5 +19,13 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup")){
+            hasPowerUp = true;
+            Destroy(other.gameObject);
+        }
     }
 }
